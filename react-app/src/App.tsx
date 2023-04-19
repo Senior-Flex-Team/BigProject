259d314e01cec28b04bf687c844a22afa086b55b
dmitriysenko <dmitriy.senko.job@gmai.com>
2023-04-19 15:33:23 +0300

    First changes

---
+++ react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CardsContainer from "./components/CardsContainer/CardsContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <CardsContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/CardsContainer/CardsContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './CardsContainer.module.css';
+
+const CardsContainer = () => {
+
+
+
+  let arrayOfCards = [];
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default CardsContainer;
