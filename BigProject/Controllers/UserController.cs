a47fd99ab348e38bb0318364768b4430dd56a8a9
zharov <zharov01.work@gmail.com>
2023-04-21 18:24:08 +0300

    ops(https://bigprojectteam.atlassian.net/jira/software/projects/BP/boards/1?selectedIssue=BP-4) : task is done

---
+++ BigProject/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using BigProject.Data;
+using BigProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BigProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly ApiDbContext _context;
+        public UserController(
+            ILogger<UserController> logger,
+            ApiDbContext context) 
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet("GetAllUsers")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var user = new User()
+                {
+                    Name = "Jack",
+                };
+
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+
+                var allDrivers = _context.Users.ToListAsync();
+
+                return Ok(allDrivers);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
--- BigProject/Program.cs
+++ BigProject/Program.cs
@@ -1,18 +1,31 @@
+using BigProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//AddDbContext дл€ регистрации контекста базы данных ApiDbContext в качестве сервиса в контейнере зависимостей приложени€.
+//ћы указываем, что мы хотим использовать поставщик базы данных PostgreSQL дл€ нашего контекста базы данных, использу€ метод UseNpgsql. “акже мы передаем строку подключени€, полученную из конфигурационного файла, в метод UseNpgsql,
+//чтобы установить подключение к базе данных.
+builder.Services.AddDbContext<ApiDbContext>(options => options.UseNpgsql(connection));
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyOrigin()
+.AllowAnyHeader()
+.AllowAnyMethod());
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.MapControllers();
 
