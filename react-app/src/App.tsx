e9f9271f7fc31aa499cf72b7d44ff4e487f82f86
dmitriysenko <dmitriy.senko.job@gmai.com>
2023-04-28 00:15:22 +0300

    Added Header

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css';
 import CardsFeedContainer from "./components/CardsFeedContainer/CardsFeedContainer";
+import Header from "./components/Header/Header";
 
 function App() {
 
   return (
-    <div className="App">
+    <>
+      <Header></Header>
       <CardsFeedContainer/>
-    </div>
+    </>
   );
 }
 
