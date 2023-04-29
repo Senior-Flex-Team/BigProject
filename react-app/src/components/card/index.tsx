10d6bb8d08e0e429cc09251655219d104f85f84f
Yury Katasonov <katasonov-yury@yandex.ru>
2023-04-29 19:54:52 +0300

    🐞 fix bugs via ESLint and format via Prettier 🐞

---
+++ react-app/src/components/card/index.tsx
@@ -0,0 +1,49 @@
+import { type FC } from 'react';
+
+import { type ICard } from '../../entities/card';
+
+import styles from './card.module.css';
+
+interface Properties {
+    card: ICard;
+}
+
+export const Card: FC<Properties> = ({ card }) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div className={styles.authorImg}>
+                    <img
+                        src={card.authorImg}
+                        alt=''
+                    />
+                </div>
+                <p className={styles.info}>
+                    <span className={styles.authorName}>{card.authorName}</span>{' '}
+                    in
+                    <span className={styles.category}>{card.categoryName}</span>
+                    <span className={styles.divider}></span>
+                    <span className={styles.date}>{card.createdAt}</span>
+                </p>
+            </div>
+            <div className={styles.body}>
+                <div className={styles.contentWrapper}>
+                    <div className={styles.title}>{card.title}</div>
+                    <p className={styles.content}>{card.content}</p>
+                </div>
+                <div className={styles.imageWrapper}>
+                    <div className={styles.image}>
+                        <img
+                            src={card.img}
+                            alt=''
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className={styles.footer}>
+                <span className={styles.tag}>{card.tags[0]}</span>
+                <span className={styles.timeToRead}>{card.timeToRead}</span>
+            </div>
+        </div>
+    );
+};
--- react-app/src/entities/Card.ts
+++ react-app/src/entities/Card.ts
@@ -1,12 +1,12 @@
-export interface Card {
-  id: string,
-  title: string,
-  content: string
-  authorName: string,
-  authorImg: string,
-  categoryName: string,
-  createdAt: string,
-  tags: string[],
-  timeToRead: string,
-  img: string
+export interface ICard {
+    id: string;
+    title: string;
+    content: string;
+    authorName: string;
+    authorImg: string;
+    categoryName: string;
+    createdAt: string;
+    tags: string[];
+    timeToRead: string;
+    img: string;
 }
