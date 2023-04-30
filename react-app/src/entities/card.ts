2e8646116d0b445fbe42a5185e2d88cb076b59ae
Yury Katasonov <katasonov-yury@yandex.ru>
2023-04-30 22:43:48 +0300

    fix card format style

--- react-app/src/entities/card.ts
+++ react-app/src/entities/card.ts
@@ -1,12 +1,12 @@
 export interface ICard {
-    id: string;
-    title: string;
-    content: string;
-    authorName: string;
-    authorImg: string;
-    categoryName: string;
-    createdAt: string;
-    tags: string[];
-    timeToRead: string;
-    img: string;
+  id: string;
+  title: string;
+  content: string;
+  authorName: string;
+  authorImg: string;
+  categoryName: string;
+  createdAt: string;
+  tags: string[];
+  timeToRead: string;
+  img: string;
 }
