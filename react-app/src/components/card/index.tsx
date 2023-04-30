6d3d7b933b44a92dba42515ac7d325b5eacafecf
Yury Katasonov <katasonov-yury@yandex.ru>
2023-04-30 16:23:13 +0300

    add card decompose

--- react-app/src/components/card/index.tsx
+++ react-app/src/components/card/index.tsx
@@ -3,6 +3,9 @@ import { type FC } from 'react';
 import { type ICard } from '../../entities/card';
 
 import styles from './card.module.css';
+import { CardHeader } from './card-header';
+import { CardBody } from './card-body';
+import { CardFooter } from './card-footer';
 
 interface Properties {
   card: ICard;
@@ -11,38 +14,9 @@ interface Properties {
 export const Card: FC<Properties> = ({ card }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.authorImg}>
-          <img
-            src={card.authorImg}
-            alt=''
-          />
-        </div>
-        <p className={styles.info}>
-          <span className={styles.authorName}>{card.authorName}</span> in
-          <span className={styles.category}>{card.categoryName}</span>
-          <span className={styles.divider}></span>
-          <span className={styles.date}>{card.createdAt}</span>
-        </p>
-      </div>
-      <div className={styles.body}>
-        <div className={styles.contentWrapper}>
-          <div className={styles.title}>{card.title}</div>
-          <p className={styles.content}>{card.content}</p>
-        </div>
-        <div className={styles.imageWrapper}>
-          <div className={styles.image}>
-            <img
-              src={card.img}
-              alt=''
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles.footer}>
-        <span className={styles.tag}>{card.tags[0]}</span>
-        <span className={styles.timeToRead}>{card.timeToRead}</span>
-      </div>
+      <CardHeader card={card} />
+      <CardBody card={card} />
+      <CardFooter card={card} />
     </div>
   );
 };
